0ee4395b7c8a527fe1e80309730fddc90df8efbc
Victor Anderssén <victor.anderssen@gmail.com>
2026-01-21 19:43:27 +0200

    Fix: test coverage ignore the generated Slurm client

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-EXCLUDE_REGEX='/cmd/|/internal/api/types|/internal/api/server'
+EXCLUDE_REGEX='/cmd/|/internal/api/types|/internal/api/server|internal/slurmclient'
 PKGS=$(go list ./... | grep -vE "$EXCLUDE_REGEX")
 
 go test $PKGS -cover -coverprofile=coverage.out
